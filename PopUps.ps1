983b73c174616943dbe32daa5da02857d95ae305
Izzy Martinez <122241717+Izzy0827@users.noreply.github.com>
2025-02-24 15:29:58 -0600

    Update PopUps.ps1
    - added Label objects ln26 + ln8
    - added .Refresh() method ln43

--- PopUps.ps1
+++ PopUps.ps1
@@ -3,9 +3,10 @@
 Add-Type -AssemblyName System.Windows.Forms #allos us to create a GUI on windows
 
 $Screens = @() ##array to hold win form objects
-$num = 100 ##num of pop-ups 
+$num = 10 ##num of pop-ups 
 $Sound = New-Object System.Media.SoundPlayer ##object needed to play error sound
-
+$font = New-Object System.Drawing.Font("Microsoft Sans Serif", 20) ##variable used to store font object | first parameter will be font style | secomd will be font size 
+ 
 ##custom functtion that returns a random number that we will use for pop-up (x,y) coordinates
 Function rand() 
 {
@@ -15,12 +16,22 @@ Function rand()
 ##for loop that creates all win form custom objects
 for($x=0; $x -lt $num; $x++)
 {
-    $Screen = New-Object System.Windows.Forms.Form
+    $Screen = New-Object System.Windows.Forms.Form ##create new win form object
     $Screen.ClientSize = '300 , 120' ##change these to custom size of win form
     $screen.StartPosition = "Manual" ##allows us to custom location 
     $Screen.Location = "$(rand),$(rand)" ##this is where rand() function comes in handy
     $Screen.Text = "ERROR" ##chsnge name of pop-up here
     $Screen.BackColor = "Red" ##change color here 
+
+    $Label = New-Object System.Windows.Forms.Label ##create new label object used for msg inside win form pop-up
+    $Label.Text = 'Insert Message' ##change msg here 
+    $Label.Location = New-Object System.Drawing.Size(50,45) ##location where msg will be displayed inside win form pop-up
+    $Label.Size = '200,100' #size of area where msg will cover | change to you liking | (x,y) format
+    $Label.Font = $font #font variable created in line 8
+    
+    
+    $Screen.Controls.add($Label)
+    
     $Screens += $Screen #this populates array of win form objects
 }
 
@@ -29,6 +40,7 @@ foreach($Screen in $Screens)
 {
     $Screen.show() #display win form
     $Sound.playsync()#play error sound
+    $Screen.Refresh()#allows win form to refresh and be displayed properly
     Start-Sleep -Seconds '0.20' #change interval of time between win forms
 }
 
